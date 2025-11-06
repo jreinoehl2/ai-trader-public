00a65906b60a316a4c40b465c3aad768c8b796a6
Jaden Reinoehl <jyr@tenaska.com>
2025-11-05 18:50:30 -0600

    Refactor API routes and improve request parsing logic in api.py; update CORS middleware settings in main.py

--- server/app/main.py
+++ server/app/main.py
@@ -6,8 +6,10 @@ app = FastAPI(title="AI Trader API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True, # change to domain in prod (would be something like 'https://jadenreinoehl/ai-trader.com')
+    allow_origins=["*"], # change to domain in prod (would be something like 'https://jadenreinoehl/ai-trader.com')
+    allow_credentials=False, 
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(api_router)
